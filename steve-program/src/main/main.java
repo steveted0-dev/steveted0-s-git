70c01548233f455a80c487fd72ed558f12003d4b
SteveTed0 <unconfigured@null.spigotmc.org>
2022-08-15 08:38:06 +0900

    요약:중간저장 편집:6(13) |_.metadata | |_.log | |_version.ini | |_.plugins | | |_org.eclipse.core.runtime | |   |_.settings | |     |_org.eclipse.jdt.ui.prefs | |_org.eclipse.e4.workbench | | |_workbench.xmi | |_org.eclipse.ui.workbench |   |_workingsets.xml |_steve-program   |_src     |_main       |_main.java

--- steve-program/src/main/main.java
+++ steve-program/src/main/main.java
@@ -1,10 +1,74 @@
 package main;
+import java.awt.*;
+import javax.swing.*;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Frame f = new Frame();
+		
+		f.setTitle("Frame Test");
+			
+		f.setSize(300,300);
+		f.setVisible(true);
 	}
 
+}
+public class WindowEventEx extends Frame implements WindowListener{
+    public WindowEventEx(){
+	super("windowEvent 테스트");
+		
+	add(exit);
+	setBounds(300,300,200,200);
+	setVisible(true);
+		
+	addWindowListener(this);
+		
+    }
+	
+    public static void main(String[] args) {
+	new WindowEventEx();
+    }
+
+    @Override
+    public void windowActivated(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+		
+    }
+
+    @Override
+    public void windowClosed(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+    }
+
+    @Override
+    public void windowClosing(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+	System.exit(0); //프로그램 종료
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+		
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+		
+    }
+
+    @Override
+     public void windowIconified(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+		
+    }
+
+    @Override
+    public void windowOpened(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+		
+    }
 }
