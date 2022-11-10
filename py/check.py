d54555a92442a84b1972b5eb97b5027864cc44c9
SteveTed0 <111440235+SteveTed0@users.noreply.github.com>
2022-11-10 18:01:23 +0900

    Update check.py

--- py/check.py
+++ py/check.py
@@ -2,6 +2,6 @@ def(sload, sstart):
   c=open(__file__ + "/##check", "r")
   j=c.readline()
   if j == "steveted0check":
-    if load == "load":
+    if sload == "load":
       import load as start
       start.start()
